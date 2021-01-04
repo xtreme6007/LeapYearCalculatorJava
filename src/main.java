10e69ae20f131adcc4b983f778ee2baf1f696890
xtreme6007 <prestonnichols96@gmail.com>
2021-01-04 12:11:31 -0600

    completed challenge and added in testing

--- src/main.java
+++ src/main.java
@@ -1,10 +1,14 @@
-public class main {
+public abstract class main {
 
-
+    public static void main(String[] args) {
     boolean test1 = leapCalc.isLeapYear(-1600);
     boolean test2 = leapCalc.isLeapYear(1600);
     boolean test3 = leapCalc.isLeapYear(2017);
     boolean test4 = leapCalc.isLeapYear(2000);
-
+    System.out.println(test1);
+    System.out.println(test2);
+    System.out.println(test3);
+    System.out.println(test4);
+}
 
 }
