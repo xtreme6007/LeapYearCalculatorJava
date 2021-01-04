3dc20840a4bd36370b30a52c464f415a6dd7cf64
xtreme6007 <prestonnichols96@gmail.com>
2021-01-04 12:01:51 -0600

    working on formula to check if parameter is leap year or not

--- src/leapCalc.java
+++ src/leapCalc.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class leapCalc {
+public class leapCalc {
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999){
+            int Even4 = year % 4;
+            int Even100 = year % 100;
+            int Even400 = year % 400;
+            if(Even4 == 0 ){
+                if (Even100 == 0) {
+                    if(Even400 == 0){
+                        return true;
+                    }
+
+                }
+            }
+
+        } else {
+            return false;
+        }
+
+
+    }
+
 }
